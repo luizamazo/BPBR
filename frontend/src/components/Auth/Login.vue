4f7f605f4274703d456da44a95a1e0f33ca0a2e8
Luiza <luizamazo@hotmail.com>
2019-12-20 22:54:58 -0400

    primeiro commit

---
+++ frontend/src/components/Auth/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <v-layout row> 
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+                            <form>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field>
+
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                            </form>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+    
