d1db47c8548276bcb35131d97495ddbf02b447e4
luizamazo <luizamazo@hotmail.com>
2020-02-14 22:47:21 -0400

    Update stacks.js

--- stacks.js
+++ stacks.js
@@ -1,40 +1,142 @@
+let fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    config = require(path.join(__dirname, 'config.js')),
+    twitter = require('./twitter/utils.js');
+    twit = new Twit(config);
+ 
+  fs.readdir(__dirname + '/media', function(err, files){
+  
+    if(err){
+        console.log(err)
+    }else{
+        let media = []
+        files.forEach(function(file){
+            media.push(file)
+        })
+    
+    master(media)
+      
+    }
+})
+
 let master = async (media) => {
-      for(const[index, file] of media.entries()){
+
+
+      mediaPath = [],
+      mediaType = '',
+      mediaIdStrings = [],
+      mediaIdStringVideo = '',
+      mediaInfo = {},
+      media_id = '',
+      tweet_id = '',
+  
+      tweet = `|POST| inferno: `
+
+     for(const[index, file] of media.entries()){
         mediaPath = path.join(__dirname, '/media/' + file) 
         mediaType = getMediaType(mediaPath)
         mediaInfo = await uploadChunkedMedia(mediaPath, mediaType)
-        
-        console.log(`
-        index: ${index},
-        tweet_id: ${tweet_id}
-        `)
+     
+    
+      if(mediaInfo.video) {
+        mediaIdStringVideo = mediaInfo.media_id_string
 
-        if(mediaInfo.video){
-          mediaIdStringVideo = mediaInfo.media_id_string
-          if(mediaPath.includes('1 -')){
-            tweet_id = await postTweet(tweet, null, mediaIdStringVideo)
+        if(mediaIdStrings.length > 0 && mediaIdStrings.length < 4){
+          if(tweet_id == ''){
+            console.log('first tweet has images')
+            await postTweet(tweet, null, mediaIdStrings)
+              .then(res => {
+                tweet_id = res
+              })
           }else{
-            tweet_id = await postTweet(tweet, tweet_id, mediaIdStringVideo)
+            console.log('tweet with images comes before any video')
+            await postTweet(tweet, tweet_id, mediaIdStrings)
+              .then(res => {
+                tweet_id = res
+              })
           }
         }
-    
+
+        if(mediaPath.includes('1 -')){
+          console.log('first tweet will contain a video')
+          await postTweet(tweet, null, mediaIdStringVideo)
+            .then(res => {
+              tweet_id = res
+            })
+        }else{
+          console.log('video is not the first one, it belongs to a thread')
+          await postTweet(tweet, tweet_id, mediaIdStringVideo)
+            .then(res => {
+              tweet_id = res
+            })
+        }
+      }else{
+        console.log('to push image media_id', mediaIdStrings)
+        mediaIdStrings.push(mediaInfo.media_id_string)
       }
+
+      if(mediaIdStrings.length == 4){
+        if(tweet_id == ''){
+          console.log('first tweet contains 4 pics')
+          await postTweet(tweet, null, mediaIdStrings)
+            .then(res => {
+              tweet_id = res
+            })
+        }else{
+          console.log('the pics belongs to a thread)
+          await postTweet(tweet, tweet_id, mediaIdStrings)
+            .then(res => {
+              tweet_id = res
+            })
+        }
+        mediaIdStrings = []
+      }
+
+      if(index == media.length - 1){
+        if(tweet_id != ''){
+          console.log('post remaining images that dont fit the ifs')
+          await postTweet(tweet, tweet_id, mediaIdStrings)
+            .then(res => {
+              tweet_id = res
+            })
+        }else{
+          await postTweet(tweet, null, mediaIdStrings)
+        }
+      }
+
+      console.log(`
+      index: ${index},
+      mediaPath: ${mediaPath} | mediaType: ${mediaType},
+      mediaId - IMG: ${mediaIdStrings},
+      mediaId - VIDEO: ${mediaIdStringVideo},
+      tweetId: ${tweet_id},
+    `)
+    
+
+    }
 }
 
- 
- 
  const uploadChunkedMedia = async (mediaPath, mediaType) => { 
   let mediaData = fs.readFileSync(mediaPath, {encoding:'base64'}),
   mediaSize = fs.statSync(mediaPath).size,
   mediaInfo = {}
 
   await initUpload(mediaSize, mediaType)
-    .then(async res1 => {
-      await appendUpload(mediaData, res1)
-      .then(async res2 => {
-        await finalizeUpload(res2)
-        .then(async res3 => {
-          mediaInfo = res3
+    .then(async mediaId => {
+      await appendUpload(mediaData, mediaId)
+      .then(async mediaId => {
+        await finalizeUpload(mediaId)
+        .then(async response => {
+          if(!response.processing_info){
+            mediaInfo = response
+          }else{
+            if(response.processing_info.state == 'pending' || response.processing_info.state == 'in_progress'){
+              await checkStatus(response.media_id_string).then(res => {
+                mediaInfo = res
+              })
+            }
+          }
         })
       })
     }) 
@@ -42,11 +144,19 @@ let master = async (media) => {
  }
 
  const initUpload = async (mediaSize, mediaType) => {
+  let mediaCategory = '' 
+  if(mediaType == 'video/mp4'){
+     mediaCategory = 'TWEET_VIDEO'
+   }else{
+     mediaCategory = 'TWEET_IMAGE'
+   }
   return await twitter.makePost('media/upload', {
     command    : 'INIT',
     total_bytes: mediaSize,
     media_type : mediaType,
+    media_category: mediaCategory
   }).then(result => {
+    console.log('media', mediaCategory)
     return result.data.media_id_string
   }).catch(err => {
     console.log('erro no init upload', err)
@@ -77,6 +187,35 @@ const finalizeUpload = async (mediaId) => {
   })
 }
 
+const checkStatus = async (mediaId) => {
+  let data = {},
+  inferno = true
+ console.log(mediaId)
+  while(inferno){
+    
+    await twitter.getSomething('media/upload', {
+      command      : 'STATUS',
+      media_id     : mediaId,
+    }).then(result => {
+     
+      console.log('checando inicialmente', result.data)
+  
+      if(result.data.processing_info.state == 'succeeded'){
+        data = result.data 
+        inferno = false
+       }else if(result.data.processing_info.state == 'failed'){
+        data = {error: 'deu ruim'} 
+        inferno = false
+       }
+    }).catch(err => {
+      console.log('erro no init upload', err)
+    })   
+  }
+ 
+  return data
+  
+}
+
 const getMediaType = (mediaPath) => {
   let mediaType = ''
   fileExtension = mediaPath.substr((mediaPath.lastIndexOf('.') + 1))
@@ -92,16 +231,37 @@ const getMediaType = (mediaPath) => {
   return mediaType
 }
 
+let postMedia = async (mediaPath, mediaType) => {
+   
+  let mediaData = fs.readFileSync(mediaPath, {encoding:'base64'}),
+      mediaSize = fs.statSync(mediaPath).size,
+      mediaId = ''
+
+      await twitter.initUpload(mediaSize, mediaType) 
+        .then(async res => {
+          await twitter.appendUpload(res, mediaData).then(async res2 => {
+            await twitter.finalizeUpload(res2).then(res3 => {
+              mediaId = res3
+            })
+          })
+        }).catch(err => {
+          console.log(err)
+        })
+        console.log('mediaID', mediaId)
+        return mediaId
+  }
+
+
 let postTweet = async (status, in_reply_to_status_id = null, media_ids = null)=> {
-  let username = '@corongabot'
-  console.log('inferno', in_reply_to_status_id)
+
   return await twitter.makePost('statuses/update', {
     status,
     in_reply_to_status_id,
-    username,
+    auto_populate_reply_metadata: true,
     media_ids
   }).then(res => {
-    console.log('tweet postado', res.data)
+    
       return res.data.id_str
     })
 }
+
